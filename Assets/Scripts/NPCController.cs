5f7a96afcc6a4fdfe03500863ac1243e972a838a
Dopplerian <dopplerian+git@proton.me>
2025-01-06 14:33:13 +0100

    stuff idek

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using UnityEngine.XR.ARFoundation;
 
 public class NPCController : MonoBehaviour
 {
     private Logger logger;
-
     public static int POINTS = 0; // Static variable to store the points
     public float timeSpawn = 3f; // NPC spawn period
     private float nextSpawn; // Time to spawn the next NPC
@@ -19,12 +19,14 @@ public class NPCController : MonoBehaviour
     public int enemyRate = 50; // Rate of enemy spawn
     public GameObject PrefabEnemy; // Prefab of the enemy NPC
     public GameObject PrefabAlly; // Prefab of the ally NPC
+    private ARPlaneManager arPlaneManager;
 
     void Start()
     {
         logger = FindObjectOfType<Logger>();
         // Set the time to spawn the next NPC
         nextSpawn = Time.time + timeSpawn;
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
     }
 
     void Update() {
@@ -36,26 +38,67 @@ public class NPCController : MonoBehaviour
     }
 
     public void SpawnAndDespawn(){
+        if (arPlaneManager == null || arPlaneManager.trackables.count == 0)
+        {
+            Debug.LogWarning("No planes detected to spawn NPCs.");
+            logger.Log("No planes detected to spawn NPCs.");
+            return;
+        }
+
         // set x position randomly for spawning the NPC and create the new location
         float xPos = UnityEngine.Random.Range(-xRangeCoord, xRangeCoord);
         Vector3 newLocation = new Vector3(transform.position.x + xPos, transform.position.y + yDisplacement, transform.position.z + zDisplacement);
+
+        // Filter only horizontal planes
+        List<ARPlane> horizontalPlanes = new List<ARPlane>();
+        foreach (var plane in arPlaneManager.trackables)
+        {
+            if (plane.alignment == UnityEngine.XR.ARSubsystems.PlaneAlignment.HorizontalUp)
+            {
+                horizontalPlanes.Add(plane);
+            }
+        }
+
+        if (horizontalPlanes.Count == 0)
+        {
+            Debug.LogWarning("No horizontal planes detected for spawning NPCs.");
+            return;
+        }
+
+        // Get a random horizontal plane
+        ARPlane selectedPlane = horizontalPlanes[UnityEngine.Random.Range(0, horizontalPlanes.Count)];
+
+        // Generate a random position within the bounds of the selected plane
+        Vector3 randomPoint = GetRandomPointOnPlane(selectedPlane);
 
         // Instantiate the NPC and destroy it after 3 seconds. Randomizing if it is an enemy or an ally
         GameObject NPC = null;
         bool isEnemy = UnityEngine.Random.Range(0, 100) <= enemyRate;
         logger.Log("New NPC created!");
         if (isEnemy) { 
-            NPC = Instantiate(PrefabEnemy, newLocation, Quaternion.identity);
+            NPC = Instantiate(PrefabEnemy, randomPoint, Quaternion.identity);
             logger.Log("This NPC is an enemy");
         }
         else { 
-            NPC = Instantiate(PrefabAlly, newLocation, Quaternion.identity);
+            NPC = Instantiate(PrefabAlly, randomPoint, Quaternion.identity);
             logger.Log("This NPC is an ally");
         }
         Destroy(NPC, 3f);
         logger.Log("NPC destroyed!");
+    }
 
+    private Vector3 GetRandomPointOnPlane(ARPlane plane)
+    {
+        // Get the plane's center and size
+        Vector3 planeCenter = plane.transform.position;
+        Vector2 planeSize = plane.size;
 
+        // Generate a random position within the plane's bounds
+        float randomX = UnityEngine.Random.Range(-planeSize.x / 2, planeSize.x / 2);
+        float randomZ = UnityEngine.Random.Range(-planeSize.y / 2, planeSize.y / 2);
+
+        // Return the random point in world space
+        return planeCenter + plane.transform.right * randomX + plane.transform.forward * randomZ;
     }
 
     public void stopGenerating()
